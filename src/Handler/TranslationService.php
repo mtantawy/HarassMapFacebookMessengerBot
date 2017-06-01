fb4756821b1c2bcd390b0f0a3493ea18b56f063d
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2017-06-01 09:23:32 +0200

    TranslationService::getLocalizedString()

--- src/Handler/TranslationService.php
+++ src/Handler/TranslationService.php
@@ -8,9 +8,24 @@ class TranslationService
     const LANG_FILES_PATH = '../lang/';
 
     protected $container;
+
+    private $loadedStrings = [];
        
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+    }
+
+    public function getLocalizedString(string $string, string $locale, string $gender = null): array
+    {
+        if (!isset($this->loadedStrings[$locale])) {
+            $this->loadedStrings[$locale] = include self::LANG_FILES_PATH . $locale . '.php';
+        }
+
+        if (is_array($this->loadedStrings[$locale][$string])) {
+            return $this->loadedStrings[$locale][$string][$gender] ?? $string;
+        }
+
+        return $this->loadedStrings[$locale][$string] ?? $string;
     }
 }
