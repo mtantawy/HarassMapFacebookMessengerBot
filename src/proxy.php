0996b96b8f695d79d03db1d68d22368c03d0ca51
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2017-06-02 23:55:54 +0200

    removing first proxy attempt

--- src/proxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $lat = $_GET['lat'];
-    $lng = $_GET['lng'];
-    echo file_get_contents('https://maps.harassmap.org/api?task=incidents&locationlatitude=' . $lat . '&locationlongitude=' . $lng);
-?>
