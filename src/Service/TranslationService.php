20e24b86327622b76d456a3361be8d9f51b2422e
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2017-06-14 20:25:16 +0200

    using default locale if no translation available for user's locale

--- src/Service/TranslationService.php
+++ src/Service/TranslationService.php
@@ -10,6 +10,11 @@ class TranslationService
     protected $container;
 
     private $loadedStrings = [];
+
+    private $availableLocales = [
+        'en_US',
+        'ar_AR',
+    ];
        
     public function __construct(ContainerInterface $container)
     {
@@ -18,6 +23,10 @@ class TranslationService
 
     public function getLocalizedString(string $string, string $locale, string $gender = null): string
     {
+        if (! in_array($locale, $this->availableLocales)) {
+            $locale = 'en_US';
+        }
+
         if (!isset($this->loadedStrings[$locale])) {
             $this->loadedStrings[$locale] = include __DIR__ . '/' . self::LANG_FILES_PATH . $locale . '.php';
         }
